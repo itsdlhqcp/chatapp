101b28a7f0cfa21805d05a40b79c24bbbd92e619
dilhaqued <dilhaqued@gmail.com>
2022-01-13 20:36:59 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+      apiKey: "AIzaSyD_doQXC6shRxqFuo3MqX80AjzdmhZ6sFM",
+      authDomain: "chat-web-app-46b89.firebaseapp.com",
+      databaseURL: "https://chat-web-app-46b89-default-rtdb.firebaseio.com",
+      projectId: "chat-web-app-46b89",
+      storageBucket: "chat-web-app-46b89.appspot.com",
+      messagingSenderId: "1054557095729",
+      appId: "1:1054557095729:web:cbe3a2083a8993efb7d210"
+    };
+
+    const app = firebase.initializeApp(config);
