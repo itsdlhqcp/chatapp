8ed7e474397d491a4f47bbf1e1775b5113efd1fa
dilhaqued <dilhaqued@gmail.com>
2022-01-20 23:24:05 +0530

    reusable reeditable profile name

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
 
     const { profile } = useProfile();
+
+    const onSave = async newData => {
+        console.log(newData);
+    };
 
     return <>
     <Drawer.Header>
@@ -15,6 +20,13 @@ const Dashboard = ({onSignOut}) => {
 
     <Drawer.Body>
        <h3>hey -{profile.name}</h3>
+       <Divider />
+       <EditableInput
+       name ="nickname"
+       initialValue={profile.name}
+       onSave={onSave}
+       label={<h6 className='mb-2'>nickname</h6>}
+        />
     </Drawer.Body>
     <Drawer.Footer>
         <Button block color='red' onClick={onSignOut} />
